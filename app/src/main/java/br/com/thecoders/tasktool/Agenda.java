b0490815c63bc014599221c32aef6d9c4df120ab
thecodersoriginal <44100404+thecodersoriginal@users.noreply.github.com>
2018-10-12 20:36:32 -0300

    app

---
+++ app/src/main/java/br/com/thecoders/tasktool/Agenda.java
@@ -0,0 +1,15 @@
+package br.com.thecoders.tasktool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Agenda extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda);
+    }
+}
